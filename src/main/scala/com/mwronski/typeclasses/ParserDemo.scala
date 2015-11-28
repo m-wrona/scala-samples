06db1306c4427ad59bf583903f3fbe1f804e5edd
Michal Wronski <michal.robert.wronski@gmail.com>
2015-11-28 08:36:05 +0100

    Another sample added to type classes.

--- src/main/scala/com/mwronski/typeclasses/ParserDemo.scala
+++ src/main/scala/com/mwronski/typeclasses/ParserDemo.scala
@@ -4,6 +4,7 @@ object ParserDemo {
 
   def main(args: Array[String]) {
     shapelessModelParsing
+    println("--------------")
     modelParsing
   }
 
@@ -13,6 +14,11 @@ object ParserDemo {
     println(s"Parsed person using shapeless type classes: ${person}")
     val wrongPerson = ShapelessModelParser[Person]("Michal,wrong")
     println(s"Person parsing error using shapeless type classes: ${wrongPerson}")
+
+    val book = ShapelessModelParser[Book]("Fall of Giants,Ken Follett,2010")
+    println(s"Parsed book using shapeless type classes: ${book}")
+    val wrongBook = ShapelessModelParser[Person]("Fall of Giants,2010,Ken Follett")
+    println(s"Person book error using shapeless type classes: ${wrongBook}")
   }
 
   def modelParsing = {
@@ -21,6 +27,11 @@ object ParserDemo {
     println(s"Parsed person using type classes: ${person}")
     val wrongPerson = ModelParser[Person]("Michal,wrong")
     println(s"Person parsing error using type classes: ${wrongPerson}")
+
+    val book = ModelParser[Book]("Fall of Giants,Ken Follett,2010")
+    println(s"Parsed book using type classes: ${book}")
+    val wrongBook = ModelParser[Person]("Fall of Giants,2010,Ken Follett")
+    println(s"Person book error using type classes: ${wrongBook}")
   }
 
 
